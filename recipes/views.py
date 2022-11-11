a827e5e5b0616abf8a48b3f0aec81762000c988b
Alex Vido <alex.vido93@gmail.com>
2022-11-11 17:44:18 -0300

    Added CSS, Header, Footer,  Recipes and Others

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 # HTTP REQUEST
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Alex Vido',
+    })
     # HTTP RESPONSE
