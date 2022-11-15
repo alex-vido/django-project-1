20b9581844d26c50efe46ec3f2df4be6dd08d6fd
Alex Vido <alex.vido93@gmail.com>
2022-11-15 12:57:56 -0300

    Added fake text and photos.

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 # HTTP REQUEST
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Alex Vido',
+        'recipes': [make_recipe() for _ in range(10)],
     })
     # HTTP RESPONSE
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-views.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
