e958c69d33167e248340a2ea28fc65eb81eaf17a
Alex Vido <alex.vido93@gmail.com>
2022-11-17 11:53:55 -0300

    Include recipe title, refactored recipe_is_html

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import get_list_or_404, render
-
-from utils.recipes.factory import make_recipe
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from .models import Recipe
 
@@ -31,7 +29,12 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(
+        Recipe, id=id,
+        is_published=True,
+    )
+
     return render(request, 'recipes/pages/recipe-views.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
